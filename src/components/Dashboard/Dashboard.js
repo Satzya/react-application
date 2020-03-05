3682932e7f7f0d2b82db641007f12083c1a820e4
Satzya <satyabratachakraborty275@gmail.com>
2020-03-05 17:30:02 +0530

    Changes done in form data

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -11,14 +11,22 @@ class Dashboard extends React.Component {
             buttonName: 'Add Record',
             active: "active",
             navTabs: ["Home", "Profile", "Account"],
-            dashHiddenFlag: false,
-            profileHiddenFlag: true,
+            inputType: "text",
             sectionHeading: "Plan Journey",
+            currentValue: "",
             journeyDetails: {
-                sourceLocation: "Source",
-                destLocation: "Destination",
+                inputPlaceHolder: "Source",
+                date: "Date"
+            },
+            formData: {
+                sourceInput: "",
+                destInput: "",
+                dateInput: ""
             },
             steps: "Step 1",
+            dashHiddenFlag: false,
+            profileHiddenFlag: true,
+            inputTextHiddenFlag: false,
             backButtonDisabled: true
         }
     }
@@ -50,19 +58,90 @@ class Dashboard extends React.Component {
         }
     }
 
-
-    setChanges = (event) => {
-        console.log(event.currentTarget.classList)
+    counter = 0
+    getNextData = (event) => {
+        event.preventDefault()
+        switch (this.counter) {
+            case 0: {
+                this.setState({
+                    journeyDetails: {
+                        ...this.state.journeyDetails,
+                        inputPlaceHolder: "Destination"
+                    }, steps: "Step 2", currentValue: this.state.formData.destInput, backButtonDisabled: false
+                })
+                this.counter += 1;
+                console.log(this.counter)
+                break;
+            } case 1: {
+                this.setState({
+                    journeyDetails: {
+                        ...this.state.journeyDetails,
+                        inputPlaceHolder: "Date"
+                    }, steps: "Step 3", currentValue: this.state.formData.dateInput, inputType: "date"
+                })
+                this.counter += 1
+                break;
+            } case 2: {
+                this.setState({ inputTextHiddenFlag: true, steps: "Review Particulars" })
+                this.counter += 1
+            }
+        }
+    }
+    getBackData = (event) => {
+        event.preventDefault()
+        console.log(this.counter)
+        switch (this.counter) {
+            case 3: {
+                this.setState({ steps: "Step 3", inputType: "date", inputTextHiddenFlag: false, currentValue: this.state.formData.dateInput })
+                this.counter -= 1
+                break;
+            }
+            case 2: {
+                this.setState({
+                    journeyDetails: {
+                        ...this.state.journeyDetails,
+                        inputPlaceHolder: "Destination"
+                    }, steps: "Step 2", inputType: "text", inputTextHiddenFlag: false, currentValue: this.state.formData.destInput
+                })
+                this.counter -= 1
+                break;
+            }
+            case 1: {
+                this.setState({
+                    journeyDetails: {
+                        ...this.state.journeyDetails,
+                        inputPlaceHolder: "Source"
+                    }, steps: "Step 1", backButtonDisabled: true, currentValue: this.state.formData.sourceInput
+                })
+                this.counter -= 1
+                break;
+            }
+        }
+    }
+    setFormData = (inputBlock, event) => {
         this.setState({
-            journeyDetails: Object.assign({}, this.state.journeyDetails, {
-                sourceLocation: this.state.journeyDetails.destLocation
-            })
+            formData: {
+                ...this.state.formData,
+                [inputBlock]: event.target.value /* In order to pass key element as a parameter we use [...] instead of directly assigning the params */
+            }
         })
-        this.setState({ steps: "Step 2" })
     }
 
-    jj = () => {
-        this.setState({ backButtonDisabled: false })
+    evaluateInput = (event) => {
+        console.log(event.target.value);
+        this.setState({ currentValue: event.target.value })
+        switch (event.target.name) {
+            case "Source": {
+                this.setFormData("sourceInput", event)
+                break;
+            } case "Destination": {
+                this.setFormData("destInput", event)
+                break;
+            } case "Date": {
+                this.setFormData("dateInput", event)
+                break;
+            }
+        }
     }
 
     dashBoardContent = () => {
@@ -71,10 +150,17 @@ class Dashboard extends React.Component {
                 <h1 className="User-data">{this.state.sectionHeading}</h1>
                 <div hidden={this.state.dashHiddenFlag}>
                     <label>{this.state.steps}</label><br />
-                    <input type="text" placeholder={this.state.journeyDetails.sourceLocation} /><br />
                     <div>
-                        <button className="btn btn-primary backButtonPlacer" disabled={this.state.backButtonDisabled} onClick={this.setChanges}>Back</button>
-                        <button className="btn btn-primary nextButtonPlacer" onClick={this.jj}>Next</button>
+                        <form>
+                            <input type={this.state.inputType} placeholder={this.state.journeyDetails.inputPlaceHolder} name={this.state.journeyDetails.inputPlaceHolder} onChange={this.evaluateInput} value={this.state.currentValue} hidden={this.state.inputTextHiddenFlag} /><br />
+                            <div hidden={!this.state.inputTextHiddenFlag}>
+                                <h3><label>Source: {this.state.formData.sourceInput}</label></h3><br />
+                                <h3><label>Destination: {this.state.formData.destInput}</label></h3><br />
+                                <h3><label>Journey: {this.state.formData.dateInput}</label></h3><br />
+                            </div>
+                            <button className="btn btn-primary backButtonPlacer" disabled={this.state.backButtonDisabled} onClick={this.getBackData}>Back</button>
+                            <button className="btn btn-primary nextButtonPlacer" onClick={this.getNextData}>Next</button>
+                        </form>
                     </div>
                 </div>
             </div>
